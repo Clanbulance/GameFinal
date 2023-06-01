f061548b0f910261b54a38e9ed294ed8617fde21
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2023-06-01 16:00:08 +0200

    right optins now load, disable buttons

--- script.js
+++ script.js
@@ -12,24 +12,34 @@ paperRound = () => {
 sciccorsRound = () => 
     playround(getComputerChoice(),"sciccors");
 
+//this resets the boxes every time
+//Narrowed down all the .getElementById's to 1 QuerySelector!!!!
+function resetMakeUp(){
+    //document.getElementById("computerRock").style.backgroundColor= "white";
+    //document.getElementById("paperStart").style.backgroundColor= "white";
+    //document.getElementById("computerPaper").style.backgroundColor= "white";
+    //document.getElementById("rockStart").style.backgroundColor= "white";
+    //document.getElementById("computerSciccors").style.backgroundColor= "white";
+    //document.getElementById("sciccorsStart").style.backgroundColor= "white";
+    document.querySelectorAll(".playerChoices input, .computerChoices a").forEach(elem => {elem.style.backgroundColor= "white";});
+    document.getElementById("resultMessage").innerHTML = "The first one to win 5 times wins the game";
+}
+
 // this function can take all input buttons 
 function playround(a,b){
-    document.getElementById("computerRock","paperStart").style.backgroundColor= "white";
-    document.getElementById("paperStart").style.backgroundColor= "white";
-    document.getElementById("computerPaper").style.backgroundColor= "white";
-    document.getElementById("rockStart").style.backgroundColor= "white";
-    document.getElementById("computerSciccors").style.backgroundColor= "white";
-    document.getElementById("sciccorsStart").style.backgroundColor= "white";
+    resetMakeUp();
     round(a,b);
     console.log("Playerscore:"+ " " +  playerScore + " " + "-----" + " Computerscore:" + " " + computerScore);
     if (playerScore === 5 ){
             document.getElementById("resultMessage").innerHTML= "You're a god";
-            document.getElementById("playAgain").innerHTML= "Play Again"
-            console.log(" Winner winner Chicken dinner")
+            document.getElementById("playAgain").innerHTML= "Play Again";
+            console.log(" Winner winner Chicken dinner");
+            document.querySelectorAll(".playerChoices input").forEach(elem => {elem.disabled = true;});
         } else if (computerScore === 5){
             document.getElementById("resultMessage").innerHTML= "You're dogshit";
-            document.getElementById("playAgain").innerHTML= "Try Again"
-            console.log("The computer is smarter then you")
+            document.getElementById("playAgain").innerHTML= "Try Again";
+            console.log("The computer is smarter then you");
+            document.querySelectorAll(".playerChoices input").forEach(elem => {elem.disabled = true;});
          }
         }
 
@@ -51,6 +61,12 @@ function getPlayerSelection(){
 let computerScore = 0;
 let playerScore = 0;
 
+//  if tie
+//  if computereslection is rock * ppsel is rock, make rock blue 
+//
+//
+
+
     //start  round(getComputerChoice(),getPlayerSelection()) generates fresh values every game
 function round(computerSelection,playerSelection){
     computerSelection = computerSelection.toLowerCase();
@@ -58,14 +74,21 @@ function round(computerSelection,playerSelection){
       if (computerSelection === playerSelection){
                 console.log("                It's a tie!            ")
                 document.getElementById("resultMessage").innerHTML= "TIE";
+                if (computerSelection === "rock" && playerSelection === "rock"){
+                    document.querySelectorAll("#computerRock, #rockStart").forEach(elem => {elem.style.backgroundColor= "blue";})
+                } else if (computerSelection === "paper" && playerSelection === "paper"){
+                    document.querySelectorAll("#computerPaper, #paperStart").forEach(elem => {elem.style.backgroundColor= "blue";})
+                } else if (computerSelection === "sciccors" && playerSelection === "sciccors") {
+                    document.querySelectorAll("#computerSciccors, #sciccorsStart").forEach(elem => {elem.style.backgroundColor= "blue";})
+                }
                 return "It's a tie!";
+
 
         }   else if ( computerSelection === "rock" && playerSelection == "paper"){
                 document.getElementById("computerRock").style.backgroundColor= "red";
                 document.getElementById("paperStart").style.backgroundColor= "green";
                  playerScore = playerScore +1;
                  document.getElementById("playerScoree").innerHTML= playerScore;
-              
                 return "You Win!";
 
 
@@ -88,7 +111,16 @@ function round(computerSelection,playerSelection){
         }   else {
                 computerScore = computerScore +1;
                 document.getElementById("computerScoree").innerHTML= computerScore;
-               
+                if (computerSelection === "paper" && playerSelection === "rock"){
+                    document.getElementById("rockStart").style.backgroundColor = "red";
+                    document.getElementById("computerPaper").style.backgroundColor = "green";
+                } else if (computerSelection === "rock" && playerSelection === "sciccors"){
+                    document.getElementById("computerRock").style.backgroundColor = "green";
+                    document.getElementById("sciccorsStart").style.backgroundColor = "red";
+                } else if (computerSelection === "sciccors" && playerSelection === "paper") {
+                    document.getElementById("computerSciccors").style.backgroundColor = "green";
+                    document.getElementById("paperStart").style.backgroundColor = "red";  
+                }
                 return computerSelection +  " beats"+" " + playerSelection + ", you lose!";
     }
 }
